2018a15917dd31a8b8bb3d3a4bb56c7f998ed6bd
UbaldoBD <ubaldobancy526@gmail.com>
2022-10-16 19:46:02 -0300

    MVT Ubaldo

---
+++ home/models.py
@@ -0,0 +1,10 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+
+# Create your models here.
+
+class familiar(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    edad = models.IntegerField()
+    fecha_nacimiento = models.DateField(null=True)
--- proyectoDjango/urls.py
+++ proyectoDjango/urls.py
@@ -22,5 +22,9 @@ urlpatterns = [
     path('fecha/', views.fecha),
     path('fecha-nacimiento/<int:edad>', views.calcular_fecha_nacimiento),
     path('mi-template/', views.mi_template),
+    path('mi-template/<str:nombre>', views.tu_template),
+    path('prueba-template/', views.prueba_template),
+    path('ver-familiares/', views.ver_familiares),
+    path('crear-familiar/<str:nombre>/<str:apellido>/', views.crear_familiar),
     path('admin/', admin.site.urls),
 ]
